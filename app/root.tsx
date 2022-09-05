930efdf4f3fe425922cf0f6de28fa798ad999c98
François Schuers <francois.schuers@gmail.com>
2022-09-05 20:04:11 +0200

    Install Tailwind css

--- app/root.tsx
+++ app/root.tsx
@@ -8,12 +8,17 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import { getInstalledInjectedConnectors, StarknetProvider } from "@starknet-react/core";
+import styles from "./styles/app.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Carbonable",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }]
+}
 
 export default function App() {
   const connectors = getInstalledInjectedConnectors();
