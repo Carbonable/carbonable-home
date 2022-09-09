afb0233c017e6bee9162eb1a3ef32129d7fb0c9d
François Schuers <francois.schuers@gmail.com>
2022-09-09 14:03:28 +0200

    Start header bg + text

--- app/components/Link/MenuLink.tsx
+++ app/components/Link/MenuLink.tsx
@@ -6,7 +6,7 @@ interface LinkProps {
     className?: string
 }
 
-const defaultCssClass = 'text-beaige transition duration-600 ease-out delay-200 hover:text-green ';
+const defaultCssClass = 'text-beaige transition duration-500 ease-out delay-100 hover:text-green ';
 
 export default function MenuLink({ href, children, className }: LinkProps) {
     return <Link to={href} className={defaultCssClass + className}>{children}</Link>;
