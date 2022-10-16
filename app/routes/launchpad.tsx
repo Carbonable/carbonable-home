c4b15abece07d85495046cd33346ba596de38d2b
François Schuers <francois.schuers@gmail.com>
2022-10-16 16:10:59 +0200

    Retrieve starknet id from fetcher

---
+++ app/routes/launchpad.tsx
@@ -0,0 +1,38 @@
+import { useAccount, useConnectors } from '@starknet-react/core';
+import { useEffect, useState } from 'react';
+import { fetchStarnetId } from '~/utils/starknetid';
+
+
+export default function Launchpad() {
+    const { address, status } = useAccount();
+    const { connect, connectors } = useConnectors();
+    const [starnetId, setStarnetId] = useState("");
+
+    useEffect(() => {
+        getStarnetId();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [status]);
+
+    async function getStarnetId() {
+        const starknetId = await fetchStarnetId(address);
+        setStarnetId(starknetId);
+    }
+
+    return (
+        <>
+            <p>Status: {status}</p>
+            <p>Wallet address: {address}</p>
+            <p>Starknet ID: {starnetId}</p>
+            <div>
+                {connectors.map((connector) => (
+                    connector.available() && status === 'disconnected' ?
+                    <li key={connector.id()}>
+                        <button onClick={() => connect(connector)}>
+                            Connect {connector.name()}
+                        </button>
+                    </li> : null
+                ))}
+            </div>
+        </>
+    )
+}
