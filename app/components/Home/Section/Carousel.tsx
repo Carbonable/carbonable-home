70da51a21808eb1741610175c55fd89d837570cf
François Schuers <francois.schuers@gmail.com>
2022-11-16 17:52:07 +0100

    feature(front): Add animation from Lord. Change configuration for fly and remove unnecessary starknet dependencies

--- app/components/Home/Section/Carousel.tsx
+++ app/components/Home/Section/Carousel.tsx
@@ -85,44 +85,46 @@ export default function Carousel() {
     ];
     
     return (
-        <div id="assets" className="w-11/12 max-w-screen-2xl scroll-mt-12 mx-auto mt-8 lg:mt-12 xl:mt-24">
-            <div className="flex items-start justify-center">
-                <PlusIconWhite className="w-8 md:w-12"></PlusIconWhite>
-                <h1 className="w-10/12 items-center uppercase font-trash text-bold text-xl md:text-3xl lg:text-4xl xl:text-5xl text-center">FUTURE-PROOF NFT<br/>INVESTMENTS</h1>
-                <PlusIconWhite className="w-8 md:w-12"></PlusIconWhite>
-            </div>
-            <h2 className="text-center font-inter mb-8">Break free from old beliefs</h2>
-            <Slider ref={slider => (slidz = slider)} {...settings} className="w-10/12 mx-auto md:mt-8">
-                {slides.map((image, index) => (
-                    <div key={`image_${index}`} className="px-2  outline-0">
-                        <img alt={`Carbonable project ${index}`} onClick={() => handleClick(index)} src={`/assets/images/home/${image.name}`} className={index === activeSlide ? "rounded-lg brightness-110 cursor-pointer" : "rounded-lg brightness-50 cursor-pointer"} />
-                    </div>
-                ))}
-            </Slider>
-            <div className="flex flex-wrap mt-8 text-center lg:text-left lg:w-10/12 lg:mx-auto lg:flex-nowrap">
-                <div className="flex w-full items-center justify-center lg:w-9/12 lg:justify-start lg:flex-wrap">
-                   <div className="flex w-full items-center justify-center lg:justify-start">
+        <div className="preventOverflow">
+            <div id="assets" className="w-11/12 max-w-screen-2xl scroll-mt-12 mx-auto mt-8 lg:mt-12 xl:mt-24">
+                <div className="flex items-start justify-center">
+                    <PlusIconWhite className="w-8 md:w-12"></PlusIconWhite>
+                    <h1 className="w-10/12 items-center uppercase font-trash text-bold text-xl md:text-3xl lg:text-4xl xl:text-5xl text-center">FUTURE-PROOF NFT<br/>INVESTMENTS</h1>
+                    <PlusIconWhite className="w-8 md:w-12"></PlusIconWhite>
+                </div>
+                <h2 className="text-center font-inter mb-8">Break free from old beliefs</h2>
+                <Slider ref={slider => (slidz = slider)} {...settings} className="w-10/12 mx-auto md:mt-8">
                     {slides.map((image, index) => (
-                        <SliderButton key={`button_${index + 1}`} selected={index === activeSlide} onClick={() => handleClick(index)}>0{index + 1}</SliderButton>
+                        <div key={`image_${index}`} className="px-2  outline-0">
+                            <img alt={`Carbonable project ${index}`} onClick={() => handleClick(index)} src={`/assets/images/home/${image.name}`} className={index === activeSlide ? "rounded-lg brightness-110 cursor-pointer" : "rounded-lg brightness-50 cursor-pointer"} />
+                        </div>
                     ))}
-                   </div>
-                    <div className="w-full hidden lg:block lg:ml-1">
+                </Slider>
+                <div className="flex flex-wrap mt-8 text-center lg:text-left lg:w-10/12 lg:mx-auto lg:flex-nowrap">
+                    <div className="flex w-full items-center justify-center lg:w-9/12 lg:justify-start lg:flex-wrap">
+                    <div className="flex w-full items-center justify-center lg:justify-start">
+                        {slides.map((image, index) => (
+                            <SliderButton key={`button_${index + 1}`} selected={index === activeSlide} onClick={() => handleClick(index)}>0{index + 1}</SliderButton>
+                        ))}
+                    </div>
+                        <div className="w-full hidden lg:block lg:ml-1">
+                            {slides[activeSlide].subtitle}
+                        </div>
+                    </div>
+                    
+                    <div className="w-full flex items-center justify-center font-inter text-3xl line-through mt-4 lg:w-4/12 lg:justify-end lg:text-right lg:min-h-[96px] md:text-4xl lg:text-5xl">
+                        {slides[activeSlide].title}
+                    </div>
+                    <div className="w-full lg:hidden min-h-[52px]">
                         {slides[activeSlide].subtitle}
                     </div>
                 </div>
-                
-                <div className="w-full flex items-center justify-center font-inter text-3xl line-through mt-4 lg:w-4/12 lg:justify-end lg:text-right lg:min-h-[96px] md:text-4xl lg:text-5xl">
-                    {slides[activeSlide].title}
-                </div>
-                <div className="w-full lg:hidden min-h-[52px]">
-                    {slides[activeSlide].subtitle}
+                <div className="flex items-center justify-center lg:justify-start lg:w-10/12 mx-auto mt-8">
+                    <LinkButton href="#simulators" className="bg-green text-xs md:text-sm text-center min-w-[190px] before:content-['Estimate\00a0your\00a0yield']" children={undefined}></LinkButton> 
+                    <LinkOutsideButton href={MEDIUM_LINK} className="bg-lightblue text-xs md:text-sm ml-2">Learn more</LinkOutsideButton>
                 </div>
             </div>
-            <div className="flex items-center justify-center lg:justify-start lg:w-10/12 mx-auto mt-8">
-                <LinkButton href="#simulators" className="bg-green text-xs md:text-sm text-center min-w-[190px] before:content-['Estimate\00a0your\00a0yield']" children={undefined}></LinkButton> 
-                <LinkOutsideButton href={MEDIUM_LINK} className="bg-lightblue text-xs md:text-sm ml-2">Learn more</LinkOutsideButton>
-            </div>
-      </div>
+        </div>
     )
 }
 
--- app/components/Home/Section/Video.tsx
+++ app/components/Home/Section/Video.tsx
@@ -1,6 +1,7 @@
 import { PlayIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 import { ModalVideo } from "~/components/Modal";
+import Image from "~/components/Image"
 
 
 export default function Video() {
@@ -11,22 +12,41 @@ export default function Video() {
       };
 
     return (
-        <>
-            <div id="video" className="z-0 w-full min-h-[50vh] bg-home-video bg-no-repeat bg-cover mt-16 flex flex-wrap items-center xl:min-h-[70vh]">
-                <div className="w-7/12 pl-6 md:w-1/2 md:pl-16 lg:pl-36 xl:pl-52 2xl:pl-[25%]">
+        <div className="relative oHidden">
+            <Image 
+                src="/assets/images/home/home-header-bg.jpg"
+                w={1920}
+                h={1080}
+                className="background clip-video"
+            />
+            <div id="video" className="w-full min-h-[50vh] mt-16 flex flex-wrap items-center xl:min-h-[70vh] xl:w-[1136px] mx-auto">
+                <div 
+                    className="w-11/12 mx-auto md:ml-[8.33%] md:w-5/12 lg:w-[556px]"
+                    id="sticky-card-end"
+                >
                     <h1 className="uppercase font-trash text-bold text-lg leading-5 md:text-3xl lg:text-4xl xl:text-5xl text-left">FIGHT CLIMATE CHANGE</h1>
                     <h2 className="uppercase font-americana text-bold text-green leading-5 text-lg md:text-3xl lg:text-4xl xl:text-5xl text-left">GET HIGH & GROWING YIELD</h2>
                     <div className="font-inter mt-4 text-xs leading-5 md:text-sm lg:text-base 2xl:max-w-[90%]">Carbonable empowers anyone to invest in the greatest challenge of our time, by financing nature’s regeneration.</div>
                 </div>
-                <div className="w-5/12 md:w-1/2">
-                    <div className="relative w-full flex items-center justify-center md:w-1/2 mx-auto 2xl:mr-[40%]">
-                        <img src="/assets/images/home/play-button-text.svg" alt="play button background" className="w-[122px] animate-spin-slow md:w-[216px] xl:w-[312px]" />
-                        <img src="/assets/images/home/play-button-bg.png" alt="play button background" className="absolute w-[72px] mt-[1px] md:w-[124px] xl:w-[190px]" />
-                        <PlayIcon className="w-10 absolute mt-1 ml-1 xl:w-20 hover:text-green cursor-pointer" onClick={() => setOpen(true)} />
-                    </div>
-                </div>
+            </div>
+            <div className="md:mt-[-10%] 2xl:mt-[0] relative w-full flex items-center justify-center md:w-1/2 mx-auto">
+                <img src="/assets/images/home/play-button-text.svg" alt="play button background" className="w-[122px] animate-spin-slow md:w-[216px] xl:w-[312px]" />
+                <img src="/assets/images/home/play-button-bg.png" alt="play button background" className="absolute w-[72px] mt-[1px] md:w-[124px] xl:w-[190px]" />
+                <PlayIcon className="w-10 absolute mt-1 ml-1 xl:w-20 hover:text-green cursor-pointer" onClick={() => setOpen(true)} />
             </div>
             <ModalVideo open={open} handleClose={handleClose} />
-        </>
+
+            <svg width="0" height="0">
+                <defs>
+                    <clipPath
+                        transform="scale(0.0007293946024799417, 0.0011111111111111111)"
+                        clipPathUnits="objectBoundingBox"
+                        id="clip-video"
+                    >
+                        <path d="M658.994 0H101.558c-12.68 0-22.96 10.28-22.96 22.96v88.348a22.96 22.96 0 01-10.338 19.18l-57.921 38.115A22.96 22.96 0 000 187.782v200.203a22.96 22.96 0 0010.164 19.064l35.023 23.507A22.96 22.96 0 0155.35 449.62v427.42c0 12.681 10.28 22.96 22.96 22.96H1348.04c12.68 0 22.96-10.279 22.96-22.96V595.725c0-12.68-10.28-22.959-22.96-22.959h-59.25c-12.68 0-22.96-10.28-22.96-22.96V461.73c0-7.815 3.98-15.094 10.55-19.318l71.89-46.179a22.95 22.95 0 0010.55-19.318V52.099c0-12.68-10.28-22.96-22.96-22.96H718.689a22.962 22.962 0 01-12.193-3.505l-35.309-22.13A22.96 22.96 0 00658.994 0z" fill="#C4C4C4" />
+                    </clipPath>
+                </defs>
+            </svg>
+        </div>
     )
 }
